72c160d3d9528533fa4c760ba97094c437b9c4b8
yoyan1 <yoyan947@gmail.com>
2024-03-12 01:09:56 -0700

    dashboard students

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -1,48 +1,74 @@
 <script setup>
 import {ref, computed} from 'vue'
 
+const gradeLevels = ref(['Grade I', 'Grade II', 'Grade III', 'Grade IV', 'Grade V', 'Grade VI'])
 const students = ref([
     {
-        id: '632272',
+        id: '6322721',
         profile: './images/helloCutie.jpg',
         name: 'Roland Clarion',
-        gradeLvl: 'Grade III',
+        gradeLvl: 'III',
         lrn: '7462548723',
         barangay: 'Guadalupe',
         street: 'So. Mabuni',
-        zone: '1'
+        zone: '1',
+        gender: 'male',
+        section: 'kamatis'
     },
     {
         id: '7323772',
         profile: './images/helloCutie.jpg',
         name: 'Rosmarie Panilagao',
-        gradeLvl: 'Grade III',
+        gradeLvl: 'III',
         lrn: '7462548723',
         barangay: 'Guadalupe',
         street: 'So. Mabuni',
-        zone: '1'
+        zone: '1',
+        gender: 'female',
+        section: 'buongon'
     },
     {
-        id: '32674237',
+        id: '3267423',
         profile: './images/helloCutie.jpg',
         name: 'Christian Mark Nepa',
-        gradeLvl: 'Grade III',
+        gradeLvl: 'III',
         lrn: '7462548723',
         barangay: 'Guadalupe',
         street: 'So. Mabuni',
-        zone: '1'
+        zone: '1',
+        gender: 'male',
+        section: 'manga'
     }
 ])
 
+const key = ref(0);
+const isShow = ref(false);
+const notFound = ref(true)
+
+function show(id){
+    key.value = id;
+}
 const searchInput = ref('')
 
 // Define a computed property to filter students based on search query
 const filteredStudents = computed(() => {
   const search = searchInput.value.toLowerCase()
   return students.value.filter(student =>
-    student.name.toLowerCase().includes(search) || student.id.includes(search)
+    student.name.toLowerCase().includes(search) || student.lrn.includes(search)
   )
 })
+
+// Define a computed property to filter grade based on search query
+const filteredLevel = computed(() => {
+  const level = filtered.value.toLowerCase()
+  return gradeLevels.value.filter(grdLvl =>
+    grdLvl.toLowerCase().includes(level)
+  )
+})
+
+if (!filteredStudents) {
+    notFound.value = !notFound.value
+}
 </script>
 <template>
     <div class="container">
@@ -54,32 +80,89 @@ const filteredStudents = computed(() => {
                     <input type="search" v-model="searchInput" placeholder="Enter ID or Name">
                 </div>
             </div>
-                <div class="list">
+            <div class="filter">
+                <select name="" id="" v-model="filtered">
+                    <option value="" v-for="gradeLevel in filteredLevel">{{ gradeLevel }}</option>
+                </select>
+            </div>
+                <table>
+                    <tr>
+                        <th>Profile</th>
+                        <th>name</th>
+                        <th>LRN</th>
+                        <th>Barangay</th>
+                        <th>Street</th>
+                    </tr>
+                    <tr v-for="(student, index) in filteredStudents" class="list" v-if="notFound">
+                        <td><img src="./images/helloCutie.jpg" width="40px" alt=""></td>
+                        <td>
+                            <h4>{{ student.name }}</h4>
+                            <p>Grade {{ student.gradeLvl }}</p>
+                        </td>
+                        <td><p>{{ student.lrn }}</p></td>
+                        <td><p>{{ student.barangay }}</p></td>
+                        <td><p>{{ student.street }}</p></td>
+                    </tr>
+                    <tr v-if="!notFound">
+                        <td></td>
+                        <td><p>No data found</p></td>
+                        <td><p>No data found</p></td>
+                        <td><p>No data found</p></td>
+                        <td><p>No data found</p></td>
+                    </tr>
+                </table>
+                <!-- <div class="list">
                     <div class="label">
-                        <div width="100px">id</div>
                         <div width="50px">Profile</div>
                         <div  width="200px">Name</div>
                         <div width="100px">LRN</div>
                         <div  width="100px">Barangay</div>
                         <div  width="50px">Zone</div>
                     </div>
-                    <div class="students" v-for="student in filteredStudents">
-                        <div width="100px">{{ student.id }}</div>
-                        <div width="50px"><img src="./images/helloCutie.jpg" width="40px" alt=""></div>
-                        <div width="200px" style="text-align: left"><h4>{{ student.name }}</h4><p>{{ student.gradeLvl }}</p></div>
-                        <div width="100px">{{ student.lrn }}</div>
-                        <div width="100px">{{ student.barangay }}</div>
-                        <div width="50px">{{ student.zone }}</div>
+                    <div class="students" v-for="(student, index) in filteredStudents" @click="show(index)">
+                        <div><img src="./images/helloCutie.jpg" width="40px" alt=""></div>
+                        <div class="name"><h4>{{ student.name }}</h4><p>{{ student.gradeLvl }}</p></div>
+                        <div class="label1">{{ student.lrn }}</div>
+                        <div class="label1">{{ student.barangay }}</div>
+                        <div class="label1">{{ student.zone }}</div>
                     </div>
-                </div>
+                </div> -->
         </div>
         <div class="side">
             <div class="person-info">
-
+                <h1>Students Information</h1>
+                <div class="profile">
+                    <img src="./images/helloCutie.jpg" width="80px" alt="">
+                    <h2>{{ students[key].name }}</h2>
+                    <p>Stud id: {{ students[key].id }}</p>
+                </div>
+                <div class="details">
+                    <h3>Basic Details</h3>
+                    <div>
+                        <p class="label-details">Grade Level</p>:
+                        <p class="info-details">{{ students[key].gradeLvl }}</p>
+                    </div>
+                    <div>
+                        <p class="label-details">Section</p>:
+                        <p class="info-details">{{ students[key].section }}</p>
+                    </div>
+                    <div>
+                        <p class="label-details">Gender</p>:
+                        <p class="info-details">{{ students[key].gender }}</p>
+                    </div>
+                    <div>
+                        <p class="label-details">LRN No.</p>:
+                        <p class="info-details">{{ students[key].lrn }}</p>
+                    </div>
+                    <div>
+                        <p class="label-details">Address</p>:
+                        <p class="info-details">{{ students[key].barangay }}, {{ students[key].street }}</p>
+                    </div>
+                </div>
             </div>
-            <div class="logs">
+            <!-- <div class="logs">
 
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
@@ -105,7 +188,7 @@ const filteredStudents = computed(() => {
 
 .person-info{
     background: white;
-    height: 50vh;
+    height: 81vh;
     border-radius: 3px;
     box-shadow:  0 5px 5px #80808080;
 }
@@ -141,32 +224,109 @@ const filteredStudents = computed(() => {
     outline: 0;
     border: 0;
 }
-table{
-    text-align: center;
-}
 
-table tr{
-    border-bottom:1px solid gray;
-}
 
-table tr td{
-    text-align: center;
-    padding: 10px 5px;
-}
-
-table tr td:hover{
-    background: #126cc0;
-    color: white;
-}
-
-.list .label{
+/* .list .label{
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
+    padding: 0 20px;
 }
 .list .students{
     display: flex;
-    justify-content: space-around;
-    text-align: left;
+    justify-content: space-between;
+    align-items: center;
+    gap: 20px;
+    padding: 10px 20px;
+}
 
+.students:hover{
+    background: #3da1ff;
+}
+
+.students .label1{
+    text-align: center;
+    width: 90px;
+    max-width: 90px;
+}
+
+.students .name{
+    width: 200px;
+}
+
+.name h4{
+    font-size: 15px;
+}
+
+.name p{
+    font-size: 12px;
+} */
+
+table tr th{
+    color: gray;
+    font-family: 100;
+}
+
+table tr th, tr td{
+    text-align: left;
+    padding:10px 10px ;
+    outline: 0;
+}
+
+tr th, p{
+    color: #8f8e8e;
+}
+
+table .list:hover{
+    background: #3da1ff;
+    border: none;
+    color: white;
+}
+
+.person-info h1{
+    padding: 20px;
+    font-size: 20px;
+    font-weight: 400;
+}
+
+.profile{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+}
+
+.profile img{
+    border-radius: 100%;
+}
+
+.profile h2{
+    font-size: 20px;
+}
+
+.profile p{
+    color: gray;
+}
+
+.details {
+    padding: 10px 20px;
+    display: grid;
+    gap: 10px;
+}
+
+.details h3{
+    font-size: 15px;
+}
+
+.details div{
+    display: flex;
+    gap: 10px;
+}
+
+.details div p{
+    font-size: 12px;
+    color: gray;
+}
+.details div .label-details{
+    width: 100px;
 }
 </style>
